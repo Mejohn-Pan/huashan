01f0f0754fb50c5bb470effc9a22fa6bb8035de5
Mejohn-Pan <13932105637@163.com>
2020-04-04 22:41:06 +0800

    添加文件

---
+++ untitled/src/com/itheima/git.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @author pan
+ * @version l.0
+ * @className git
+ * @date 2020/4/4 22:33
+ * @description TODO
+ **/
+public class git {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
